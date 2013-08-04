806655e4b24ed854d8a723797b21b1ab2986da7d
kgh <mail.kennyhoang@gmail.com>
2013-08-04 00:33:26 -0500

    Added SPDY and TSL support for node servers.

--- source/app.js
+++ source/app.js
@@ -1,28 +1,50 @@
-//module to start the server
+// module to start the server
 'use strict';
+
+// dependencies
+var http = require('http');
+var spdy = require('spdy');
 var express = require('express');
 var app = express();
 var routes = require('./routes');
-var path = require('path');
-app.enable('strict routing');
+var fs = require('fs');
+
+// app settings
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.set('http port', 1337);
+app.set('https port', 1338);
 
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname)));
 
-//routers
-// app.get('/node', function(request, response) {
-// 	var body = 'Hello World';
-// 	response.setHeader('Content-Type', 'text/plain');
-// 	response.setHeader('Content-Length', body.length);
-// 	response.send(body);
-// });
+// static url for developement with /node address
+app.use('/node', express.static(__dirname));
+
+// static url for domain wide routing
+app.use(express.static(__dirname));
+
+// development only
+if ('development' === app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+// routers
 app.get('/', routes.index);
 app.get('/node', routes.index);
-var port = 1337;
-app.listen(port);
-console.log('Starting a server on port: ' + port);
+
+// http server
+http.createServer(app).listen(app.get('http port'), function() {
+	console.log('Starting a server on port: ' + app.get('http port'));
+});
+
+// SPDY server 
+var options = {
+	key: fs.readFileSync('/kadmin/server/nginx/ssl/server.key'),
+	cert: fs.readFileSync('/kadmin/server/nginx/ssl/server.cert')
+};
+spdy.createServer(options, app).listen(app.get('https port'), function() {
+	console.log('Starting a SPDY server listening on port: ' + app.get('https port'));
+});
