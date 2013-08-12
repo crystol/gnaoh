a8f1ffb052ab1614fd7eb0ef19e8dd78e9004573
Kenny <kenny@gnaoh.com>
2013-08-12 15:52:42 -0500

    Fix https routing..

--- source/router.js
+++ source/router.js
@@ -3,12 +3,13 @@ var routes = ['404', 'index', 'about', 'gallery', 'videos'];
 routes.forEach(function (value) {
     //exports a module per route
     module.exports[value] = function (req, res) {
-        if(process.env.ENV === 'PROD' && !req.secure){
-            return res.redirect(301, 'https://'+req.host+req.path);
+        if (process.env.ENV === 'PROD' && !req.secure) {
+            res.redirect(301, 'https://' + req.host + req.path);
+        } else {
+            res.render(value, {
+                title: value,
+                pretty: true
+            });
         }
-        res.render(value, {
-            title: value,
-            pretty: true
-        });
     };
 });
