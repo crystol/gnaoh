0700b03fc4dc34982b0567174275056e4ad47af1
Kenny <kenny@gnaoh.com>
2013-10-05 16:03:52 -0700

    Removed tasks folder and ignored nodemon file.

--- tasks/CSStoJade.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-fs.writeFile("./build/views/style.jade", "Hey there!", function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
-    }
-});
