bfba4175d301651f0cc1aa15f888e717de317bb4
Kenny <kenny@gnaoh.com>
2013-11-14 23:32:48 -0800

    Relocated views directories to match their routes.

---
+++ source/routes/photos.js
@@ -0,0 +1,8 @@
+// Photo galleries
+module.exports = [{
+    path: 'projects/photos/inline',
+    title: 'Inline Gallery',
+}, {
+    path: 'projects/photos/isotope',
+    title: 'Isotopic Gallery',
+}];
---
+++ source/routes/videos.js
@@ -0,0 +1,8 @@
+// Videos
+module.exports = [{
+    path: 'projects/videos/multi',
+    title: 'Multi Videos',
+}, {
+    path: 'projects/videos/single',
+    title: 'Single Video',
+}];
